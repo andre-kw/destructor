32f22f595b83855a0a820de9357cc96dbe532896
Andre K. W <andre.k.willie@gmail.com>
2019-07-01 19:28:25 -0400

    added footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
+import { AppProvider } from '../contexts/AppContext';
 import { LinkedListPath } from '../routes/LinkedListPage';
 import { BinaryTreePath } from '../routes/BinaryTreePage';
 import { StackPath } from '../routes/StackPage';
@@ -7,38 +8,42 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div id="root-container">
-        <header>
-          <section className="header-left">
-            <h1>destructor.</h1>
-            <h2>
-              <Switch>
-                <Route exact path="/" render={() => 'Linked list'} />
-                <Route exact path="/structure/linked-list" render={() => 'Linked list'} />
-                <Route exact path="/structure/stack" render={() => 'Stack'} />
-                <Route exact path="/structure/binary-tree" render={() => 'Binary tree'} />
-              </Switch>
-            </h2>
-          </section>
-          
-          <section className="header-right">
-            <nav>
-              <Link to="/structure/linked-list" className="nav-ll">Linked list</Link>
-              <Link to="/structure/stack" className="nav-stack">Stack</Link>
-              <Link to="/structure/binary-tree" className="nav-bst">Binary search tree</Link>
-            </nav>
-          </section>
-        </header>
+    const PLACEHOLDER = <nav>
+      <Link to="/structure/linked-list" className="nav-ll">Linked list</Link>
+      <Link to="/structure/stack" className="nav-stack">Stack</Link>
+      <Link to="/structure/binary-tree" className="nav-bst">Binary search tree</Link>
+    </nav>;
 
+    return <>
+      <header>
+        <section className="header-left">
+          <h1>destructor.</h1>
+          <h2>
+            <Switch>
+              <Route exact path="/" render={() => 'Linked list'} />
+              <Route exact path="/structure/linked-list" render={() => 'Linked list'} />
+              <Route exact path="/structure/stack" render={() => 'Stack'} />
+              <Route exact path="/structure/binary-tree" render={() => 'Binary tree'} />
+            </Switch>
+          </h2>
+        </section>
+        
+        <section className="header-right"></section>
+      </header>
+
+      <AppProvider>
         <Switch>
           <Route exact path="/" component={LinkedListPath} />
           <Route exact path="/structure/linked-list" component={LinkedListPath} />
           <Route exact path="/structure/stack" component={StackPath} />
           <Route exact path="/structure/binary-tree" component={BinaryTreePath} />
         </Switch>
-      </div>
-    );
+      </AppProvider>
+
+      <footer>
+        <p>© 2019 Andre Willie.</p>
+      </footer>
+    </>;
   }
 }
 
